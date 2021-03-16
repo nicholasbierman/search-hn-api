67edc4a029e70fa8a0b652c17e405da4564b0da3
Nick Bierman <biermannick83@gmail.com>
2021-03-16 11:31:50 -0400

    add basic styling for <Story />

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,17 +1,21 @@
-
+import "./story.css";
 
 export const Story = ({ title, url, points, author, num_comments }) => {
-    return (
-        <div className="Story_container">
-            <div className="Story_title">
-                <span>{title}</span>
-                {url && <a href={url}>({url})</a>}
-            </div>
-            <div className="Story_meta">
-                Points: {points} Author: {author} Comments: {num_comments}
-            </div>
+  return (
+    <article className="Story">
+      <div className="Story_container">
+        <div className="Story_data">
+          <div className="Story_title">
+            <span>{title}</span>
+            {url && <a href={url}>({url})</a>}
+          </div>
+          <div className="Story_meta">
+            {points} Points | {author} | {num_comments} Comments
+          </div>
         </div>
-    );
+      </div>
+    </article>
+  );
 };
 
 export default Story;
