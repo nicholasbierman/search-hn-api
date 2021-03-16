1a473e5be47b0efc4afc22d3f8cb7ea84f481afc
Nick Bierman <biermannick83@gmail.com>
2021-03-16 10:36:07 -0400

    create SearchResults component

---
+++ src/components/SearchResults/index.js
@@ -0,0 +1,11 @@
+
+
+export const SearchResults = () => {
+    return (
+        <section className="SearchResults">
+            <div className="SearchResults__container">
+                
+            </div>
+        </section>
+    )
+}
